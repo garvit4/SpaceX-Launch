336d6aac05ed12694048bab7fc90dae491ae78e4
Garvit <garvitarora1995@gmail.com>
2020-08-21 01:09:54 +0530

    Updates

---
+++ precache-manifest.7fed9df0bc97c3e0922e8b9c2a50d808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1066644e8eddb7c1808e54856a714335",
+    "url": "/SpaceX-Launch/index.html"
+  },
+  {
+    "revision": "34fae448e13cd857e1cf",
+    "url": "/SpaceX-Launch/static/css/main.492263c3.chunk.css"
+  },
+  {
+    "revision": "958cf61b1b858d40e106",
+    "url": "/SpaceX-Launch/static/js/2.425df803.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SpaceX-Launch/static/js/2.425df803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fae448e13cd857e1cf",
+    "url": "/SpaceX-Launch/static/js/main.4d944676.chunk.js"
+  },
+  {
+    "revision": "0fd2616392b89d8c21cb",
+    "url": "/SpaceX-Launch/static/js/runtime-main.767c403f.js"
+  }
+]);
